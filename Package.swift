89a15adad6b33b5f5bcbc62273f8299b47ab8540
Mahmoud Amer <mahmoud.amer@addapptr.com>
2025-04-11 14:37:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "RTBSPM",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "RTBSPM", targets: ["RTBSPM_core", "OMSDK"]),
